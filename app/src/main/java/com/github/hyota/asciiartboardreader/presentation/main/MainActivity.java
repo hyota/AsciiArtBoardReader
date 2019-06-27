794761f8f4616cf19dd7532e23af99f75e122140
tenth <tenth151151@gmail.com>
2019-06-27 23:55:37 +0900

    ・パッケージ構成変更

--- app/src/main/java/com/github/hyota/asciiartboardreader/presentation/main/MainActivity.java
+++ app/src/main/java/com/github/hyota/asciiartboardreader/presentation/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.github.hyota.asciiartboardreader;
+package com.github.hyota.asciiartboardreader.presentation.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.hyota.asciiartboardreader.R;
 
 public class MainActivity extends AppCompatActivity {
 
